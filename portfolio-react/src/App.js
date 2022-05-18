ba4d9e6dc7f244fa6536ac60032dda581f40b6e1
Prathibha <pratibha.indel@gmail.com>
2022-05-18 10:49:53 -0400

    imported router,route from react-router-dom and imported pages ,components  ,set the routes

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    import React from "react";
+    import { HashRouter as Router, Route } from "react-router-dom";
+    import AboutMePage from "./pages/AboutMePage"
+    import PortfolioPage from "./pages/PortfolioPage"
+    import ContactMePage from "./pages/ContactMePage"
+    import ResumePage from "./pages/ResumePage"
+    import Navbar from "./components/Navbar"
+ 
+
+    
+    function App() {
+      return (
+        <Router>
+         
+          <main>
+          <Route exact path="/"  component={AboutMePage} />
+          <Route path="/about"  component={AboutMePage} />
+          <Route path="/portfolio" component={PortfolioPage} />
+          <Route path="/contact" component={ContactMePage} />
+          <Route path="/resume" component={ResumePage} />
+          </main>
+          <Navbar/>
+
+
+          </Router >
+      
+
+
+      );
+    }
 
 export default App;
