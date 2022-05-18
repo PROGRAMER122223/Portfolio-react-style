54577c022a0b0762a42e899e0f51d36b741134a4
Prathibha <pratibha.indel@gmail.com>
2022-05-18 11:05:52 -0400

    edited navbar elements position

--- portfolio-react/src/components/Navbar/index.js
+++ portfolio-react/src/components/Navbar/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
-import Resume from "../../assets/resume/"
+
+
 
 function Background(props){
     return(
+        
         <div className="uk-position-relative">
             <div className="uk-position-top">
                 <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar = "true">
-                    <div className="uk-navbar-right">
+                    <div className="uk-navbar-center">
+                 
                         <ul className="uk-navbar-nav uk-visible@m">
                             <li><Link to = "/about">About Me</Link></li>
                             <li><Link to = "/portfolio">Portfolio</Link></li>
@@ -16,7 +19,8 @@ function Background(props){
                             <li><Link to = "/contact">Contact</Link></li>
                         </ul>
                       </div>
-                </nav>
+                      
+                 </nav>
             </div>
         </div>    
     )
