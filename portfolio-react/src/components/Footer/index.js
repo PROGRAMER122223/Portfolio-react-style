35bfcb36f5c193b9168f3034397c7e09e861be1b
Prathibha <pratibha.indel@gmail.com>
2022-05-19 11:58:48 -0400

    debugging warning

--- portfolio-react/src/components/Footer/index.js
+++ portfolio-react/src/components/Footer/index.js
@@ -5,9 +5,9 @@ function Footer(){
     return(
         <footer className="uk-text-center uk-text-middle footer uk-position-relative">
               <div className="uk-text-center uk-text-middle">
-                            <a uk-icon="icon: github-alt ; ratio: 1.5" className="contact-icon contact-link-icon" href="https://github.com/programer122223"  target="_blank"></a>
+                            <a uk-icon="icon: github-alt ; ratio: 1.5" className="contact-icon contact-link-icon" href="https://github.com/programer122223"></a>
                             <a uk-icon="icon: mail ; ratio: 1.5" className="contact-icon contact-link-icon" href="mailto:pratibha.indel@gmail.com"></a>
-                            <a uk-icon="icon: linkedin ; ratio: 1.5" className="contact-icon contact-link-icon" href="https://www.linkedin.com/in/xxx/" target="_blank"></a>
+                            <a uk-icon="icon: linkedin ; ratio: 1.5" className="contact-icon contact-link-icon" href="https://www.linkedin.com/in/xxx/"></a>
                         </div>
         </footer>        
     )
